e29e108e605db410e34720124571106f02cb0dff
David-vargasV <4523@holbertonschool.com>
2022-09-20 13:19:26 -0500

    Updated_states

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -8,8 +8,6 @@ from models import storage
 
 @app_views.route('/states', methods=['GET'], strict_slashes=False,
                  defaults={'state_id': None})
-
-
 @app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False,)
 def get_states(state_id):
     """Retrieves a State object"""
